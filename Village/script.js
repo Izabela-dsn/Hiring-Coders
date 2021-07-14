591e9f65510b237fe63ea9f20c36b355d0ef2b94
Izabela <izabeladasilva10@gmail.com>
2021-07-14 13:59:30 -0300

    guardando dados (email) no local storage

--- Village/script.js
+++ Village/script.js
@@ -1,3 +1,19 @@
+// guardar e-mails no local storage
+const emails = document.querySelector('input')
+const btnSend = document.querySelector('button')
+const errInfo = document.querySelector('.error')
+const listEmail = []
+
+btnSend.addEventListener('click', () => {
+  try {
+    listEmail.push(emails.value)
+    localStorage.setItem('listEmail', JSON.stringify(listEmail))
+  } catch {
+    errInfo.attributes.removeNamedItem('hidden')
+  }
+})
+
+// função de configuração para o slider
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 4,
   pagination: {
