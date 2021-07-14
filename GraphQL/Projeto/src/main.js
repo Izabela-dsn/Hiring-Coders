79723b1e86cc64a69a7d55e937c5f870314f77e0
Izabela <izabeladasilva10@gmail.com>
2021-07-14 18:05:18 -0300

    Renderização, parâmetros e redirecionamentos

--- GraphQL/Projeto/src/main.js
+++ GraphQL/Projeto/src/main.js
@@ -1,4 +1,7 @@
 import { createServer } from 'http'
+import { readFile } from 'fs'
+import { resolve } from 'path'
+import { parse } from 'querystring'
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1'
@@ -16,6 +19,49 @@ const server = createServer((request, response) => {
         })
       )
       response.end()
+      break
+    }
+    case '/sign-in': {
+      const filePath = resolve(__dirname, './pages/index.html')
+      readFile(filePath, (error, file) => {
+        if (error) {
+          response.writeHead(500, "Can't process HTML file")
+          response.end()
+          return
+        }
+        response.writeHead(200)
+        response.write(file)
+        response.end()
+      })
+      break
+    }
+    case '/home': {
+      const filePath = resolve(__dirname, './pages/home.html')
+      readFile(filePath, (error, file) => {
+        if (error) {
+          response.writeHead(500, "Can't process HTML file")
+          response.end()
+          return
+        }
+        response.writeHead(200)
+        response.write(file)
+        response.end()
+      })
+      break
+    }
+    case '/authenticate': {
+      let data = ''
+      request.on('data', chunk => {
+        data += chunk
+      })
+      request.on('end', () => {
+        const params = parse(data)
+        //console.log(parse(data))
+        response.writeHead(301, {
+          Location: '/home'
+        })
+        response.end()
+      })
       break
     }
     default: {
