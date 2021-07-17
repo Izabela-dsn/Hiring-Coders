6c6955b5379b789e2e5f512341415fe167cf72f9
Izabela <izabeladasilva10@gmail.com>
2021-07-17 19:54:26 -0300

    mudando para o express

--- GraphQL/Projeto/packages/server/src/main.js
+++ GraphQL/Projeto/packages/server/src/main.js
@@ -1,45 +1,20 @@
-import { createServer } from 'http'
-import { parse } from 'querystring'
+import express from 'express'
+
+const server = express()
+
+// rota que retorna status de okay
+server.get('/status', (_, response) => {
+  response.send({ status: 'Okay' })
+})
+
+server.post('/authenticate', express.json(), (request, response) => {
+  console.log('E-mail', request.body.email, 'Senha', request.body.password)
+  response.send()
+})
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1'
 
-const server = createServer((request, response) => {
-  switch (request.url) {
-    // rota que retorna status de okay
-    case '/status': {
-      response.writeHead(200, {
-        'Content-Type': 'application/json'
-      })
-      response.write(
-        JSON.stringify({
-          status: 'Okay'
-        })
-      )
-      response.end()
-      break
-    }
-
-    case '/authenticate': {
-      let data = ''
-      request.on('data', chunk => {
-        data += chunk
-      })
-      request.on('end', () => {
-        const params = parse(data)
-        //console.log(parse(data))
-
-        response.end()
-      })
-      break
-    }
-    default: {
-      response.writeHead(404, 'Not Found')
-      response.end()
-    }
-  }
-})
-
 server.listen(PORT, HOSTNAME, () => {
-  console.log(`Server is listening at http://127.0.0.1:${PORT}`)
+  console.log(`Server is listening at http://${HOSTNAME}:${PORT}`)
 })
