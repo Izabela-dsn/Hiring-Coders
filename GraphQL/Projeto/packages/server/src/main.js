b40c1b090ee0280e4161b4e4dc6c772b25ede9a5
Izabela <izabeladasilva10@gmail.com>
2021-07-21 12:09:24 -0300

    continuação GraphQL

--- GraphQL/Projeto/packages/server/src/main.js
+++ GraphQL/Projeto/packages/server/src/main.js
@@ -1,8 +1,33 @@
 import express from 'express'
 import cors from 'cors'
+import { ApolloServer, gql } from 'apollo-server-express'
+import typeDefs from './graphql/typeDefs'
 
-const server = express()
+const server =  new ApolloServer({ 
+  typeDefs,
+  resolvers
+})
 
+
+await server.start()
+const app = express()
+app.use(cors())
+
+server.applyMiddleware({
+  app,
+  cors: {
+    origin: 'http://localhost:3000 '
+  }
+})
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1'
+
+app.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is listening at http://${HOSTNAME}:${PORT}`)
+})
+
+/*
 // rota que retorna status de okay
 server.get('/status', (_, response) => {
   response.send({ status: 'Okay' })
@@ -20,10 +45,4 @@ server
       Okay: true
     })
   })
-
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000
-const HOSTNAME = process.env.HOSTNAME || '127.0.0.1'
-
-server.listen(PORT, HOSTNAME, () => {
-  console.log(`Server is listening at http://${HOSTNAME}:${PORT}`)
-})
+*/
